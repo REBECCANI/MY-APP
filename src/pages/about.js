661e758898dbf5dc843c8922e37ef99fb5393adf
REBECCANI <beckynishimwe@gmail.com>
2023-04-14 19:19:04 +0200

    this commit

---
+++ src/pages/about.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Dataset(){
+    return(
+        <div className="me">
+            <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" alt={"me"} />
+            <h1>PROFILE</h1>
+            <p>{" "}
+                Hi, I'm Andre.I am a husband to Nishimwe and currently working.I love reading books
+                 and spending time with the people that i love. I am a very honest and 
+                 trustworthy person and I value that in people. I enjoy my space a lot too.<br/>
+                 ADDRESS: KIGALI/RWANDA<br/>
+                 WIFE: NISHIMWE<br/>
+                 MUSIC-TYPE: KIZOMBA<br/>
+                 FAVORITE DAY: SUNDAY<br/>
+                 PEACEFUL LIFE: CONTROL OF TIME
+                 </p>
+        </div>
+    
+    )
+  
+}
+export default Dataset;
